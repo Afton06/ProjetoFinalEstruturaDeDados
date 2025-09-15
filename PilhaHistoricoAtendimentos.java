bc08fd1b5288a79dc2e9dc9e8a19dacd5a700788
Afton06 <ronaldvernek1234@gmail.com>
2025-09-14 22:06:15 -0300

    Create PilhaHistoricoAtendimentos.java

---
+++ PilhaHistoricoAtendimentos.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+public class PilhaHistoricoAtendimentos {
+    private Stack<Paciente> historico = new Stack<>();
+
+    public void adicionarAoHistorico(Paciente p) {
+        historico.push(p);
+    }
+
+    public Paciente verUltimoAtendido() {
+        if (!historico.isEmpty()) {
+            return historico.peek();
+        }
+        return null;
+    }
+
+    public void mostrarHistorico() {
+        if (historico.isEmpty()) {
+            System.out.println("O histórico de atendimentos está vazio.");
+            return;
+        }
+
+        System.out.println("--- Histórico de Atendimentos (do mais recente para o mais antigo) ---");
+        for (int i = historico.size() - 1; i >= 0; i--) {
+            System.out.println(historico.get(i).exibirInfo());
+        }
+    }
+}
